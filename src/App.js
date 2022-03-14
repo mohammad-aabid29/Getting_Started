aa06c174e65d9d1a847b7abb941e02e801148e7b
mohammadaabid <mohammada@crowdstaffing.com>
2022-03-14 14:26:26 +0530

    Navbar Added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,41 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto nisi alias nostrum dolorum vitae blanditiis soluta ex rerum. Aliquid sed at autem dolore, eligendi dignissimos odio officia eaque deleniti reiciendis itaque ex quae facere omnis voluptatem provident numquam, ipsa quaerat aut nobis expedita, doloremque quas quam ut! Minus ratione veniam voluptates facere qui iure expedita commodi alias repellat! Aperiam optio aliquid, repudiandae impedit nobis voluptatum vel quibusdam sed adipisci distinctio maiores nemo labore numquam fuga! Mollitia laboriosam quam numquam soluta quasi quas odio fuga ad, maiores libero rem, consequuntur, officiis blanditiis. Repellat culpa odio omnis fuga expedita amet perferendis, quidem illo repudiandae aliquid atque porro iusto, veritatis deleniti fugiat reprehenderit molestiae soluta doloremque non asperiores minima unde veniam sed quia! Magnam delectus accusamus maxime nobis iste eaque, esse commodi beatae ipsa error quaerat consectetur est molestiae ea blanditiis molestias! Explicabo quaerat magnam ipsam at neque saepe quo reiciendis omnis. Voluptatibus molestiae exercitationem dolorem dicta totam consequuntur perspiciatis inventore qui pariatur, repellendus laboriosam quo ullam accusantium, facilis tempora quas placeat cupiditate sequi eos hic eligendi at voluptates, ea saepe! Aut eveniet delectus non odio similique vitae unde nesciunt, fugiat dolor sequi fuga vel quos autem itaque facilis quas rerum nihil totam. Aut veritatis saepe perferendis nisi provident praesentium ut, sed ipsum fugiat et doloribus laborum repellendus consequatur similique dolore vitae, nesciunt maiores, aliquam id delectus minima! Aspernatur tenetur voluptates nihil iste aliquam hic expedita nostrum?</p>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem error sequi reiciendis repellendus maiores veritatis deleniti iste dolores ab. Libero excepturi ea repellat?</p>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">Link</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a className="dropdown-item" href="/">Action</a></li>
+            <li><a className="dropdown-item" href="/">Another action</a></li>
+            <li><a className="dropdown-item" href="/">Something else here</a></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" href='/'>Disabled</a>
+        </li>
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
     </div>
   );
 }
