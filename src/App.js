a41f7aa740ab0c7a8a0a72566a4340031c91f920
mohammadaabid <mohammada@crowdstaffing.com>
2022-03-14 14:18:52 +0530

    Demo Commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto nisi alias nostrum dolorum vitae blanditiis soluta ex rerum. Aliquid sed at autem dolore, eligendi dignissimos odio officia eaque deleniti reiciendis itaque ex quae facere omnis voluptatem provident numquam, ipsa quaerat aut nobis expedita, doloremque quas quam ut! Minus ratione veniam voluptates facere qui iure expedita commodi alias repellat! Aperiam optio aliquid, repudiandae impedit nobis voluptatum vel quibusdam sed adipisci distinctio maiores nemo labore numquam fuga! Mollitia laboriosam quam numquam soluta quasi quas odio fuga ad, maiores libero rem, consequuntur, officiis blanditiis. Repellat culpa odio omnis fuga expedita amet perferendis, quidem illo repudiandae aliquid atque porro iusto, veritatis deleniti fugiat reprehenderit molestiae soluta doloremque non asperiores minima unde veniam sed quia! Magnam delectus accusamus maxime nobis iste eaque, esse commodi beatae ipsa error quaerat consectetur est molestiae ea blanditiis molestias! Explicabo quaerat magnam ipsam at neque saepe quo reiciendis omnis. Voluptatibus molestiae exercitationem dolorem dicta totam consequuntur perspiciatis inventore qui pariatur, repellendus laboriosam quo ullam accusantium, facilis tempora quas placeat cupiditate sequi eos hic eligendi at voluptates, ea saepe! Aut eveniet delectus non odio similique vitae unde nesciunt, fugiat dolor sequi fuga vel quos autem itaque facilis quas rerum nihil totam. Aut veritatis saepe perferendis nisi provident praesentium ut, sed ipsum fugiat et doloribus laborum repellendus consequatur similique dolore vitae, nesciunt maiores, aliquam id delectus minima! Aspernatur tenetur voluptates nihil iste aliquam hic expedita nostrum?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem error sequi reiciendis repellendus maiores veritatis deleniti iste dolores ab. Libero excepturi ea repellat?</p>
+      </div>
     </div>
   );
 }
