72900bea72815da497e1191c304125caeef702fd
SurbhiB28 <surbhib@crowdstaffing.com>
2022-03-15 15:50:43 +0530

    Modified whole App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import FormControl from './Components/FormControl';
-import Navbar from './Components/Navbar';
-import QuotesCard from './Components/QuotesCard';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <FormControl/>
-      <QuotesCard />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
