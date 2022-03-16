cc7ab3970484401fc7197557f6fdd410c089215b
SurbhiB28 <surbhib@crowdstaffing.com>
2022-03-16 15:01:56 +0530

    Revert "Revert "Merge all 5 commits into 1""
    This reverts commit e0c662fbfe1082ad841e2ccabe7a08e927edcdff.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import FormControl from './Components/FormControl';
-import Navbar from './Components/Navbar';
-import QuotesCard from './Components/QuotesCard';
+import FormControl from "./Components/FormControl";
+import "./App.css";
+import QuotesCard from "./Components/QuotesCard";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <FormControl/>
+      I am going to make a lot of changes here. Because i want conflicts
       <QuotesCard />
+      <FormControl />
+      <Navbar />
     </div>
   );
 }
 
 export default App;
+console.log("Lot of changes made");
