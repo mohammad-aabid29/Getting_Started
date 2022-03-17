d4e9fbb353dd047564cc2c03b757a2c4f81e0110
SurbhiB28 <surbhib@crowdstaffing.com>
2022-03-17 16:54:35 +0530

    Initial commit

--- src/about.js
+++ src/about.js
@@ -20,11 +20,7 @@ function About() {
                                 <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     Dropdown
                                 </a>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a className="dropdown-item" href="/">Action</a></li>
-                                    <li><a className="dropdown-item" href="/">Another action</a></li>
-                                    <li><a className="dropdown-item" href="/">Something else here</a></li>
-                                </ul>
+                               
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link disabled" href='/'>Disabled</a>
